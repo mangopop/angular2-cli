e6a024a1a35d174e0f524bd0907154280b4edc4c
simon.norton <simon@nitritex.com>
2016-08-10 11:36:02 +0100

    using object for toggle and have strikethough for completed items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Todo';
   //we need an array to hold the todo items
-  items = ['item1', 'item2'];
+  items = [
+    {text:'item1',completed:false},
+    {text:'item2',completed:true}
+  ];
+  
+  //also need to hold state of completion
+  //could mark by array index (but this changes when new items are added)
+  completedItems: string[] = [];
+
+  //the item object is passed
+  //the strikethrough class is also added or removed
+  toggleCompleted(item:Object,index:number){
+    // console.log(`toggled with ${item}`);
+    // console.log(index);
+    // console.log(this.items[index]);
+    this.items[index].completed = !this.items[index].completed;
+  }
 
   //we need a handler push the items to the array when add is clicked or enter is pressed
   addItem(newItem:string){
     if(newItem){
-        this.items.push(newItem);        
+        this.items.push({text:newItem,completed:false});        
     }
     //we need to clear input upon entering item, cannot do this with blur nicely
   }
